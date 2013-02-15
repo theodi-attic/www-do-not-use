2627bf9f95972e31b9dd416d57eccb028632d12d
pezholio <pezholio@gmail.com>
2013-02-15 14:26:30 +0000

    Using getJSON and changing class to reflect site

--- js/training-course.js
+++ js/training-course.js
@@ -1,13 +1,7 @@
 (function($) {
-	$.ajax(
-		'http://151.236.219.131/sites/default/files/courses.json',
-		{
-			dataType: 'json',
-			success: function (data) {
-				$('.event-instances')
-					.eventTable({ data: data });
-				return true;
-			}
-		}
-	);
+	$.getJSON('/sites/default/files/courses.json', function(data) {
+		$('.views-table')
+			.eventTable({ data: data });
+		return true;
+	});
 })(jQuery);
